baca19469e8ebcec78a8f99cec08a36eb95d8306
afaq saeed <maazfarrukh@gmail.com>
2020-09-16 23:51:31 +0500

    add django heroku

--- todoproject/settings.py
+++ todoproject/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import django_heroku()
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -117,7 +117,7 @@ USE_L10N = True
 
 USE_TZ = True
 CSRF_COOKIE_SECURE = True
-SESSION_COOKIE_SECURE = True
+django_heroku.settings(locals())
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
